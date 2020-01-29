ff89993328ece5b73ce46612910adfa0a795ef8d
Jimmy Blundell <jblundell123@gmail.com>
2020-01-29 17:07:27 -0600

    Added buttons to add balls and strikes.

--- app/src/main/java/com/example/umpirebuddy/MainActivity.java
+++ app/src/main/java/com/example/umpirebuddy/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView numBalls;
+    TextView numStrikes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numBalls = findViewById(R.id.ballCounter);
+        numStrikes = findViewById(R.id.strikeCounter);
+
     }
 }
